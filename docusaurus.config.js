dc1fb2746945feef84aade373bc4fd8bee532664
Aaron LaBeau <alabeau@gmail.com>
2024-03-27 10:19:12 -0500

    publish github action

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,16 +8,18 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://cbl-ionic.dev/',
+
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/cbl-ionic-docs',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'couchbaselabs', // Usually your GitHub org/user name.
   projectName: 'cbl-ionic', // Usually your repo name.
-  deploymentBranch: 'main',
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
