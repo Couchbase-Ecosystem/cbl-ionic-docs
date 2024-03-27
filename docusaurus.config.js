b388ce8a0dac41112dc0cd7f8550ca4844021482
Aaron LaBeau <alabeau@gmail.com>
2024-03-27 11:30:45 -0500

    updated the config file to fix url error

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,11 +8,11 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'http://labs.couchbase.com/cbl-ionic-docs/',
+  url: 'http://labs.couchbase.com/',
 
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/cbl-ionic-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
