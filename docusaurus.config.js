dd8badede783d1ca3e911b3ca6ee680231482bd0
Aaron LaBeau <alabeau@gmail.com>
2024-04-02 11:36:22 -0500

    updated for cbl-ionic.dev

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,11 +8,11 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'http://labs.couchbase.com/',
+  url: 'http://cbl-ionic.dev',
 
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/cbl-ionic-docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
