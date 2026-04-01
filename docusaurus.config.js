de13b2984d48d7f5e0adf6d14d53e3a1bd243a48
Dhiraj Kumar Azad <dhirajkumar.azad@couchbase.com>
2026-04-01 14:54:14 +0530

    Add Google analytics

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,6 +24,18 @@ const config = {
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
+  headTags: [
+    {
+      tagName: 'script',
+      attributes: {},
+      innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//metrics.couchbase.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MVPNN2');`,
+    },
+  ],
+
   clientModules: [
     './src/clientModules/scarfAnalytics.js',
   ],
